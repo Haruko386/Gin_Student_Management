72dc238d358ae17ba6b8b5041cec2dc29ee1715b
Haruko386 <tryeverypossible@163.com>
2025-12-31 13:23:59 +0800

    init

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"Gin_Student_Management/models"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
 func main() {
-	gin.Default()
-	fmt.Println("Hello world")
+	// init
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+	r.Static("assets", "./assets")
+
+	err := models.InitMysql()
+	if err != nil {
+		return
+	}
+
+	defer func(db *gorm.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(models.DB)
+	models.DB.AutoMigrate()
+
+	initRoutes(r)
+
+	err = r.Run(":8081")
+	if err != nil {
+		return
+	}
 }
---
+++ models/initMysql.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
+
+func InitMysql() (err error) {
+	dsn := "root:password@(127.0.0.1:3306)/gorm_demo?charset=utf8&parseTime=True&loc=Local"
+	DB, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+	err = DB.DB().Ping()
+	return err
+}
